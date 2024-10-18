a48cee2333ec5c36d3d365a3da5c0cf330251587
Leonardo Luz <leonardoluz10@hotmail.com>
2024-10-18 19:14:19 -0300

    feat: :white_check_mark: Curso de Formik e Yup concluído.

--- src/paginas/cadastro/DadosPessoais.jsx
+++ src/paginas/cadastro/DadosPessoais.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { Tipografia } from "../../componentes/Tipografia/Tipografia";
 import { CampoTexto } from "../../componentes/CampoTexto/CampoTexto";
 import { ListaSupensa } from "../../componentes/ListaSuspensa/ListaSuspensa";
 import { Col, Row } from "react-grid-system";
 import { Botao } from "../../componentes/Botao/Botao";
 import { Link } from "react-router-dom";
-import { Form, Formik } from "formik";
-import { validations } from "../../utils/validations";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
 const estadosBrasileiros = [
   { text: "Acre", value: "AC" },
@@ -37,22 +37,65 @@ const estadosBrasileiros = [
   { text: "Sergipe", value: "SE" },
   { text: "Tocantins", value: "TO" },
 ];
+
+const formatarTelefone = (value) => {
+  if (!value) return
+  const telefone = value.replace(/\D/g, "")
+
+  return `(${telefone.slice(0, 2)}) ${telefone.slice(2, 7)}-${telefone.slice(7)}`
+}
+
+const schema = Yup.object().shape({
+  nome: Yup.string()
+    .required("Campo obrigatório")
+    .trim()
+    .lowercase()
+    .min(3, "Digite seu nome completo"),
+  nascimento: Yup.date()
+    .required("Campo obrigatório")
+    .max(new Date(), "Data inválida"),
+  estado: Yup.string()
+    .required("Campo obrigatório")
+    .max(20, "digite um estado válido"),
+  cidade: Yup.string()
+    .required("Campo obrigatório")
+    .max(20, "Cidade inválida"),
+  telefone: Yup.string()
+    .required("Campo obrigatório")
+    .matches(/^\d{11}/, "Numero de telefone inválido")
+    .transform(formatarTelefone),
+  email: Yup.string()
+    .required("Campo obrigatório")
+    .email("Email inválido"),
+  senha: Yup.string()
+    .required("Campo obrigatório")
+    .min(6, "Senha muito curta")
+    .max(12, "Senha muito longa")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{6,12}$/,
+      "Senha inválida"
+    ),
+  confirmarSenha: Yup.string()
+    .required("Campo obrigatório")
+    .oneOf([Yup.ref("senha"), null], "As senhas não conferem!"),
+  termos: Yup.boolean()
+    .oneOf([true], "É preciso aceitar os termos"),
+});
 const DadosPessoais = () => {
   return (
     <Formik
       initialValues={{
         nome: "",
+        nascimento: "",
         estado: "",
         cidade: "",
         telefone: "",
         email: "",
         senha: "",
         confirmarSenha: "",
+        termos: false
       }}
-      validate={(values) => {
-        const errors = validations(values);
-        return errors;
-      }}
+      validationSchema={schema}
       onSubmit={(values) => {
         console.log("dados do formulario", values);
       }}
@@ -71,7 +114,10 @@ const DadosPessoais = () => {
           </div>
           <Row>
             <Col>
-              <CampoTexto titulo="Nome completo" tipo="text" name="nome" />
+              <CampoTexto titulo="Nome completo" type="text" name="nome" />
+            </Col>
+            <Col>
+              <CampoTexto titulo={"Data de nascimento"} type="date" name="nascimento" />
             </Col>
           </Row>
           <Row>
@@ -79,29 +125,38 @@ const DadosPessoais = () => {
               <ListaSupensa titulo="Estado" opcoes={estadosBrasileiros} />
             </Col>
             <Col lg={8} md={8} sm={8}>
-              <CampoTexto titulo="Cidade" tipo="text" name="cidade" />
+              <CampoTexto titulo="Cidade" type="text" name="cidade" />
             </Col>
           </Row>
           <Row>
             <Col lg={6} md={6} sm={6}>
-              <CampoTexto titulo="E-mail" tipo="email" name="email" />
+              <CampoTexto titulo="E-mail" type="email" name="email" />
             </Col>
             <Col lg={6} md={6} sm={6}>
-              <CampoTexto titulo="Telefone" tipo="tel" name="telefone" />
+              <CampoTexto titulo="Telefone" type="tel" name="telefone" />
             </Col>
           </Row>
           <Row>
             <Col lg={6} md={6} sm={6}>
-              <CampoTexto titulo="Senha" tipo="password" name="senha" />
+              <CampoTexto titulo="Senha" type="password" name="senha" />
             </Col>
             <Col lg={6} md={6} sm={6}>
               <CampoTexto
                 titulo="Confirme sua senha"
-                tipo="password"
+                type="password"
                 name="confirmarSenha"
               />
             </Col>
           </Row>
+          <Row>
+            <label style={{ padding: "15px" }}>
+              <Field name="termos" type="checkbox" />
+              aceito os <Link to="#">termos de uso</Link>
+            </label>
+          </Row>
+          <ErrorMessage name="termos">
+            {(msg) => <div style={{ color: "red", marginBottom: "10px" }}>{msg}</div>}
+          </ErrorMessage>
           <Row>
             <Col lg={6} md={6} sm={6}>
               <Link to="/cadastro/interesses">
